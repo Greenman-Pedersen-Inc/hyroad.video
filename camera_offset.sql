f9d23933c2fda5ca356acdc99dcde3c7d0a2415d
colli6485 <colli6485@gmail.com>
2019-12-18 11:54:26 -0500

    Create camera_offset.sql

---
+++ camera_offset.sql
@@ -0,0 +1,8 @@
+-- use this syntax to import new rows into the database
+INSERT INTO public.camera_offset(record, gps_offset_secs, front_left_start, front_right_start, rear_right_start, rear_left_start)
+VALUES ('rec_cr634', 345600, '00:00:00', '00:00:00', '00:00:00', '00:00:00'),
+       ('rec_nj31_cr518', 345600, '00:00:00', '00:00:00', '00:00:00', '00:00:00'),
+       ('rec_nj31', 345600, '00:00:00', '00:00:00', '00:00:00', '00:00:00'),
+       ('rec_i295', 345600, '00:00:00', '00:00:00', '00:00:00', '00:00:00'),
+       ('rec_sr571', 345600, '00:00:00', '00:00:00', '00:00:00', '00:00:00'),
+       ('rec_us130_sr571', 345600, '00:00:00', '00:00:00', '00:00:00', '00:00:00');
