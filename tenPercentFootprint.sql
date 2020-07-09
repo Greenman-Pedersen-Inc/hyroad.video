f97024cbeaeac948f6af94f0036370fafed3e235
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2020-07-09 15:25:59 -0400

    Update tenPercentFootprint.sql

--- tenPercentFootprint.sql
+++ tenPercentFootprint.sql
@@ -35,3 +35,12 @@ SELECT time_stamp, x, y, z, roll, pitch, yaw, geom2d
 FROM (SELECT *, row_number() OVER(ORDER BY time_stamp ASC) AS row FROM us130_sr571) t
 WHERE t.row % 10 = 0
 ) as foo
+
+
+select "Record", ST_MakeLine(array_agg(geom)) from (
+	SELECT "Record", ST_SetSRID(ST_MakePoint("GPS (Long.) [deg]", "GPS (Lat.) [deg]"), 4326) as geom
+	FROM (SELECT "Record", "GPS (Long.) [deg]", "GPS (Lat.) [deg]", cts, row_number() OVER(ORDER BY cts ASC) AS row FROM public.gopro_gps_telemery) t
+	WHERE t.row % 100 = 0
+	order by "Record", cts
+) as points
+group by "Record"
